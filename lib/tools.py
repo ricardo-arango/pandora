9e14f68adb8ce4a259e738a9e2b6ee00ee3b9bb8
Juan S. Acero Triana <jsacero@hotmail.com>
2021-08-26 14:21:25 -0700

    Minor changes in 2010-2021.csv

--- lib/tools.py
+++ lib/tools.py
@@ -85,7 +85,9 @@ def parse_contents(contents, filename, date):
             dataloading.crime_df = pd.read_csv(io.StringIO(decoded.decode('utf-8'))).astype(dtypes)
             #df = pd.read_csv(io.StringIO(decoded.decode('utf-8')), delimiter=",", encoding="utf-8", dtype=types,
             #                       parse_dates=["FECHA"])
-            #return df
+
+            #
+
     except Exception as e:
         print(e)
         return html.Div([
@@ -103,12 +105,6 @@ def parse_contents(contents, filename, date):
 
         #html.Hr(),  # horizontal line
 
-        # # For debugging, display the raw contents provided by the web browser
-        # html.Div('Raw Content'),
-        # html.Pre(contents[0:200] + '...', style={
-        #     'whiteSpace': 'pre-wrap',
-        #     'wordBreak': 'break-all'
-        # })
     ])
 
 
@@ -117,11 +113,6 @@ def parse_contents(contents, filename, date):
               State('upload-data', 'filename'),
               State('upload-data', 'last_modified'))
 def update_output(list_of_contents, list_of_names, list_of_dates):
-    # if list_of_contents is not None:
-    #     children = [
-    #         parse_contents(c, n, d) for c, n, d in
-    #         zip(list_of_contents, list_of_names, list_of_dates)]
-    #     return children
     if list_of_contents is not None:
         children = [parse_contents(list_of_contents, list_of_names, list_of_dates)]
         return children
